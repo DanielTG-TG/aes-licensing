857b07028b1dea320a79f898a926aa40f8a7c38d
DanielTG <37558441+DanielTG-TG@users.noreply.github.com>
2021-05-14 19:39:37 -0300

    release

---
+++ Licesing1/LicesingConsole/Licesing/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Licesing;
+
+namespace Licesing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Title = "Licesing system.";
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Welcome to the licesing system.");
+
+                if (File.Exists("License.txt"))
+                {
+                    byte[] lic = File.ReadAllBytes("License.txt");
+                    Console.WriteLine("Current license: " + SymmetricEncryptor.DecryptToString(lic, "k3bTn5dQ4DD8NHh6MBgGusuRAGNorsUH"));
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Press any key to exit.");
+                    Console.ReadKey();
+                    Environment.Exit(1);
+                }
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Please input your license key.");
+                Console.Write("> ");
+
+                string input = Console.ReadLine();
+                byte[] enc = SymmetricEncryptor.EncryptString(input, "k3bTn5dQ4DD8NHh6MBgGusuRAGNorsUH");
+
+                File.WriteAllBytes("License.txt", enc);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Successfully saved the license.");
+                Console.WriteLine("Press any key to exit.");
+            }
+            catch
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("There was an error saving the license.");
+                Console.WriteLine("Press any key to exit.");
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Licesing1/MainLicesing/MainLicesing/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Licesing;
+
+namespace MainLicesing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (File.Exists("License.txt"))
+                {
+                    byte[] encrypted = File.ReadAllBytes("License.txt");
+                    string decrypted = SymmetricEncryptor.DecryptToString(encrypted, "k3bTn5dQ4DD8NHh6MBgGusuRAGNorsUH");
+                    MessageBox.Show("Licesing key: " + decrypted);
+                    //now you should verify the key in your host. Make sure to use end to end encryption.
+                    //Good luck.
+                }
+                else
+                {
+                    MessageBox.Show("License file not found. If you registered already put the file in the same folder.", "Licesing", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
